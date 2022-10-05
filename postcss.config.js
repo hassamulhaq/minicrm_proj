10879c251793b1ce992880e1a336718fab5b237c
hassamulhaq <developer.hassam@gmail.com>
2022-10-05 18:24:11 +0500

    flowbite tailwind configured

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./node_modules/flowbite/**/*.js"
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+            },
+        },
+        fontSize: {},
     },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin')
+    ],
 };
