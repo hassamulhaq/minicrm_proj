6052a5ab4213a2ab092437044166f2f9ec714d5e
hassamulhaq <developer.hassam@gmail.com>
2022-10-05 19:26:07 +0500

    Employee -mfcs created

---
+++ app/Http/Controllers/EmployeesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeesController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Employee $employee)
+    {
+    }
+
+    public function edit(Employee $employee)
+    {
+    }
+
+    public function update(Request $request, Employee $employee)
+    {
+    }
+
+    public function destroy(Employee $employee)
+    {
+    }
+}
---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class EmployeeSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
