3ef7143b9945185f48d6ece29e42bcc28911766b
hassamulhaq <developer.hassam@gmail.com>
2022-10-05 19:49:47 +0500

    Fillable, and UUID added for Models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,7 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Company extends Model
 {
+    protected $fillable = [
+        'name',
+        'email',
+        'logo'
+    ];
+
+
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Uuid::uuid4()->toString();
+            }
+        });
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,7 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Employee extends Model
 {
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'company_id',
+    ];
+
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Uuid::uuid4()->toString();
+            }
+        });
+    }
 }
