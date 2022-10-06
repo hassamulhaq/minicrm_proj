9faeafd24a50aff12f0eced48292eaec6c35cb16
hassamulhaq <developer.hassam@gmail.com>
2022-10-07 00:11:34 +0500

    Fix vite bug

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,6 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./node_modules/flowbite/**/*.js"
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-            },
-        },
-        fontSize: {},
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
     },
-
-    plugins: [
-        require('@tailwindcss/forms')
-    ],
 };
